96fe5e889cd94a59cbe0ebe41b49ed506d171f9a
rnarciso <rnarciso@gmail.com>
2021-03-27 14:50:06 -0300

    Back tester implementation in process

--- nm/__init__.py
+++ nm/__init__.py
@@ -215,7 +215,7 @@ class NMData:
         if self._nm_data is not None and 'date' in self._nm_data.columns:
             return self._nm_data.date.max().tz_localize(NM_TIME_ZONE)
         else:
-            return 'Never'
+            return pd.Timestamp(SINCE)
 
     def __repr__(self):
         return f'<NMData container class at {hex(id(self))}:\n{self._nm_data.__repr__()}' \
